ec45c8e7b57c09f675d7e36cf93b041288178ce2
Aditya Kumar Jha <mail@adijha.com>
2020-11-17 21:32:51 +0545

    loading screen updated

--- app/pages/auth/LoadingScreen.js
+++ app/pages/auth/LoadingScreen.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useCallback } from 'react'
 import { View, StyleSheet, Image } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
-import Pubg from '../../assets/icons/pubg.png'
+import Pubg from '../../assets/logo.jpeg'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  img: {
+    height: 200,
+    width: 200,
+    resizeMode: 'contain'
   }
 })
 export default function LoadingScreen(props) {
   const tryLogin = useCallback(async () => {
     // setTimeout(async () => {
-      const token = await AsyncStorage.getItem('token')
-      props.navigation.navigate(token ? 'Home' : 'SignIn')
+    const token = await AsyncStorage.getItem('token')
+    props.navigation.navigate(token ? 'Home' : 'SignIn')
     // }, 1000)
   }, [props.navigation])
   useEffect(() => {
@@ -23,7 +29,7 @@ export default function LoadingScreen(props) {
 
   return (
     <View style={styles.container}>
-      <Image source={Pubg} />
+      <Image source={Pubg} style={styles.img} />
     </View>
   )
 }
